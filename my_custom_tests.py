812f35c0895d6a523381524eeee01843d54915df
Iago <iago.brandao@viavarejo.com.br>
2023-05-13 22:13:46 -0300

    Empty train_model.py

---
+++ my_custom_tests.py
@@ -0,0 +1,20 @@
+import pytest
+import train_model
+
+def test_load_dataset_exists():
+    """
+    Testa se o método load_dataset existe no módulo train.
+
+    Este teste verificará a existência do método load_dataset no módulo train.
+    Se o método não existir, o teste falhará com uma mensagem indicando que o método load_dataset não existe.
+    """
+    assert hasattr(train_model, 'load_dataset'), "O método load_dataset não existe"
+
+def test_train_model_exists():
+    """
+    Testa se o método train_model existe no módulo train.
+
+    Este teste verificará a existência do método train_model no módulo train.
+    Se o método não existir, o teste falhará com uma mensagem indicando que o método train_model não existe.
+    """
+    assert hasattr(train_model, 'train_model'), "O método train_model não existe"
